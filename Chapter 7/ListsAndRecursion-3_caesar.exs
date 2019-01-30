5b6484b9d91550f408af056fdb738dd887701205
Marta Pozaroszczyk <Marta.Pozaroszczyk@bentley.com>
2019-01-30 09:26:56 +0100

    Adding 3rd exercise for lists and recursion (caesar)

---
+++ Chapter 7/ListsAndRecursion-3_caesar.exs
@@ -0,0 +1,14 @@
+defmodule MyList do
+	
+	def caesar([], _n), do: []
+
+	def caesar([head|tail], n) when head + n < 122 do
+		[head + n|caesar(tail, n)]
+	end
+
+	def caesar([_head|tail], n), do: ['?'|caesar(tail, n)]
+
+end
+
+IO.puts MyList.caesar('marta',5)   	# rfwyf
+IO.puts MyList.caesar('ryvkve',13)  # ???x?r
