3693dc985faddf746b8881d989091a7d4981dc4d
Marta Pozaroszczyk <Marta.Pozaroszczyk@bentley.com>
2019-02-05 16:16:55 +0100

    Adding number calculation function, chapter 11

---
+++ Ch11 StringsAndBinaries/StringsAndBinaries-4_number_calc.exs
@@ -0,0 +1,27 @@
+defmodule NumberCals do
+	
+	def calc(string), do: _calc(string) |> compute
+	def _calc(string) do
+		string
+			|> List.to_string
+			|> String.split([" "])
+	end
+
+	def compute([nr1, sign, nr2]) do
+		case sign do
+		"+" -> String.to_integer(nr1) + String.to_integer(nr2)
+		"-" -> String.to_integer(nr1) - String.to_integer(nr2)
+		"*" -> String.to_integer(nr1) * String.to_integer(nr2)
+		"/" -> if(nr2 == "0", do: "Can't divide by 0", else: String.to_integer(nr1) / String.to_integer(nr2))
+		_ 	-> "Invalid operation"
+		end
+	end
+
+end
+
+IO.inspect NumberCals.calc('123 + 666')			# 789
+IO.inspect NumberCals.calc('123 - 66')			# 57
+IO.inspect NumberCals.calc('12 * 6')			# 72
+IO.inspect NumberCals.calc('123 / 6')			# 20.5
+IO.inspect NumberCals.calc('123 / 0')			# "Can't divide by 0"
+IO.inspect NumberCals.calc('123 = 0')			# "Invalid operation"
