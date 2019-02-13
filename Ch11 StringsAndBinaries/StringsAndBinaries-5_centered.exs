f1bc752aff921a220aa35d8ed04cccb9c1be82ca
Marta Pozaroszczyk <Marta.Pozaroszczyk@bentley.com>
2019-02-13 11:51:46 +0100

    Adding working centering and non-functioning sales_tax

--- Ch11 StringsAndBinaries/StringsAndBinaries-5_centered.exs
+++ Ch11 StringsAndBinaries/StringsAndBinaries-5_centered.exs
@@ -1,10 +1,17 @@
 defmodule Centering do
+  def max_len_word(list), do: Enum.max(Enum.map(list, &String.length(&1)))
 
-	def max_len_word(list), do: Enum.max(Enum.map(list, &(String.length(&1))))
-	def centering(list) do
+  def centering(list), do: _centering(list, max_len_word(list))
 
-	end
+  def _centering([], _max_len), do: :ok
 
+  def _centering([head | tail], max_len) do
+    placeholder = String.duplicate(" ", trunc(0.5 * (max_len - String.length(head))))
+    IO.puts(placeholder <> head <> placeholder)
+    _centering(tail, max_len)
+  end
 end
 
-IO.inspect Centering.centering(["cat", "dog", "mouse", "pig"])
+IO.inspect(Centering.centering(["table", "dog", "rabbit", "rododendron"]))
+IO.inspect(Centering.centering(["Heyeeaa", "Heyeeaayeeaayeye", "I said hey? What's going on"]))
+IO.inspect(Centering.centering(["My name is", "Bong", "Jane Bong"]))
--- Ch11 StringsAndBinaries/StringsAndBinaries-7_sales_tax_calc.exs
+++ Ch11 StringsAndBinaries/StringsAndBinaries-7_sales_tax_calc.exs
@@ -1,33 +1,64 @@
 defmodule TaxCalc do
-	
-	def file_to_keyword_list() do
-		case File.open("sales_information.txt", [:read]) do
-		{:ok, file}			-> process_headers(IO.read(file, :line)) |> process_rows(file)
-		{:error, message} 	->	message
-		end
-	end
+  def file_to_keyword_list() do
+    case File.open("sales_information.txt", [:read]) do
+      {:ok, file} -> process_headers(IO.read(file, :line)) |> process_rows(file)
+      {:error, message} -> message
+    end
+  end
 
+  def process_headers(data) do
+    data
+    |> String.trim()
+    |> String.split(",")
+    |> Enum.map(&String.strip(&1))
+    |> Enum.map(&String.to_atom(&1))
+  end
 
-	def process_headers(data) do
-		data
-			|> String.trim
-			|> String.split(",")
-			|> Enum.map(&(String.strip(&1)))
-			|> Enum.map(&(String.to_atom(&1)))
-	end
+  def process_rows(header, file) do
+    value = Enum.map(IO.stream(file, :line), fn row -> single_line(row) end)
 
-	def process_rows(header, file) do
-		value = Enum.map IO.stream(file, :line), fn(row) -> single_line(row) end
-		#Enum.zip(header, value)
-	end
+    # Enum.zip(header, value)
+  end
 
-	def single_line(data) do
-		data
-			|> String.trim
-			|> String.split(",")
-			|> Enum.map(&(String.strip(&1)))
+  def single_line(data) do
+    sin_line =
+      data
+      |> String.trim()
+      |> String.split(",")
+      |> Enum.map(&String.strip(&1))
 
-	end
+    Enum.map(data, reformat_data(sin_line))
+
+    # |> replace_float
+    # |> replace_atom
+  end
+
+  def reformat_data(data) do
+    # [replace_integer(data), replace_atom(data), replace_float(data)]
+    [replace_integer(data), :NX, 123.4]
+  end
+
+  # def replace_float(data) do
+  #   data
+  #   |> Enum.take(1)
+
+  #   List.replace_at(0, Enum.take(data))
+  #   |> Enum.take()
+  # end
+
+  def replace_integer(data) do
+    {int, ""} =
+      data
+      |> List.first()
+      |> Integer.parse()
+
+    int
+  end
+
+  # def replace_atom(data) do
+  #   data
+  #   |> Enum.fetch(1)
+  # end
 end
 
-IO.inspect TaxCalc.file_to_keyword_list()
+IO.inspect(TaxCalc.file_to_keyword_list())
